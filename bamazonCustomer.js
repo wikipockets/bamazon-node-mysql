10683dadc72e9960cb1acd87ecd621a56df19848
Stephen <sg.johnson14@gmail.com>
2019-06-18 19:02:22 -0400

    tested and complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var nodemon = require("nodemon");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 8989,
+    port: 3306,
     user: "root",
     password: "root",
     database: "bamazon"
