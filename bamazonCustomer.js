982dc849589602ed1e32a51bff9367f6cab434a4
Stephen <sg.johnson14@gmail.com>
2019-06-18 17:53:45 -0400

    finished version of bamazon

---
+++ bamazonCustomer.js
@@ -0,0 +1,79 @@
+//basic connection and req
+
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var nodemon = require("nodemon");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8989,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+//connection err or display 
+connection.connect(function(err) {
+    if (err) throw err;
+    // console.log("successfully connected to MYSQL");
+    // console.log("please mysql, dammit you're a cruel mistress")
+    displayAll();
+});
+
+// Display the ids, names, and prices of all available products for sale
+function displayAll() {
+    var query = "SELECT * FROM products GROUP BY item_id";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Price: $" + res[i].price);
+        }
+        userSelection();
+    });
+}
+//function to take input from user and query the db
+function userSelection() {
+    inquirer
+        .prompt([{
+            name: "item_id",
+            type: "input",
+            message: "Please enter the Item ID of the product you would like to purchase."
+        }, {
+            name: "quantity",
+            type: "input",
+            message: "Enter the quantity you would like to purchase."
+        }])
+        .then(function(answer) {
+            var item = answer.item_id;
+            var quantity = answer.quantity;
+            var query = "SELECT * FROM products WHERE ?";
+            connection.query(query, {
+                item_id: item
+            }, function(err, res) {
+                if (err) throw err;
+                console.log("You chose item_id: " + item + " and a quantity of: " + quantity);
+                var itemDetails = res[0];
+                // show the item details
+
+                if (quantity <= itemDetails.stock_quantity) {
+                    console.log("Order successful. Number of requested items is in stock. " + (itemDetails.stock_quantity));
+                    // update database quantity
+                    var updatedQuantity = (itemDetails.stock_quantity - quantity);
+                    console.log("Updated stock quantity: " + updatedQuantity);
+                    var updateQuery = "UPDATE products SET stock_quantity = " + updatedQuantity + " WHERE item_id = " + item;
+
+                    //send to mysql
+                    connection.query(updateQuery, function(err, res) {
+                        if (err) throw err;
+
+                        console.log("Success! Quantity: " + updatedQuantity + " Past Stock Quantity: " + (itemDetails.stock_quantity));
+                    })
+                } else {
+                    console.log("We out of that");
+
+                    displayAll();
+                }
+            });
+
+        });
+}
--- data/bamazonSchema.sql
+++ data/bamazonSchema.sql
@@ -1,19 +1,21 @@
 DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
-/*well you drop it, and create it, and then use it*/
-USE bamazon
+
+/*well you drop it, and create it, and then use it...under that you have the rules for creating the database*/
+
+USE bamazon;
 
 CREATE TABLE products
 (
    item_id INT NOT NULL
    AUTO_INCREMENT,
-   product_name VARCHAR
+  product_name VARCHAR
    (100) NOT NULL,
-   department_name VARCHAR
+  department_name VARCHAR
    (100) NOT NULL,
-   price DECIMAL
+  price DECIMAL
    (10,2) NOT NULL,
-   stock_quantity INT NOT NULL,
-   PRIMARY KEY
+  stock_quantity INT NOT NULL,
+  PRIMARY KEY
    (item_id)
 );
